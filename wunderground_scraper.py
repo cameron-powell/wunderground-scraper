8171edaebb5e89079bcc41f2154039e312e28734
Cameron Powell <cameronspowell@gmail.com>
2017-10-12 00:14:16 -0400

    Created the scrape_weather_data function and cell_to_json helper function.  Updated requirements.txt to reflect new libraries used with these.
    Created the scrape_weather_data function which accepts a valid url, grabs the html from a wunderground history page, parses out the temperature information from the data table, and returns it
    as a string formatted as JSON.
    Created the helper cell_to_json function specifically to handle formatting the data we get from the data table cells, and turning it into a line to be added in a JSON object string.
    These use the beautifulsoup and requests libraries, which have been added to the requirements.txt file

--- wunderground_scraper.py
+++ wunderground_scraper.py
@@ -21,6 +21,8 @@ NOTE: This assumes you are running within an activated virtual environment
 and have the project's requirements already installed.  See the project README
 for instructions on setting that up.
 """
+from bs4 import BeautifulSoup
+import requests
 
 
 def format_location(loc):
@@ -28,6 +30,51 @@ def format_location(loc):
     return loc.strip()
 
 
+def scrape_weather_data(url):
+    """ Docstring """
+    # Get the html from the page using requests
+    response = requests.get(url)
+    if response.status_code != 200:
+        return '{"error": "Received %s status code for url: %s"}' % (response.status_code, url)
+
+    # Use beautiful soup to navigate the html to the rows in the history table
+    page = BeautifulSoup(response.text, "html.parser")
+    history_table = page.find('table', id='historyTable')
+    # Handle the case of if we weren't sent to a webpage with the table/id we want
+    if history_table is None:
+        return '{"error": "Received a link which does not contain ' \
+               'the data we want.  Link received %s"}' % url
+    history_table_rows = history_table.findAll('tr')
+
+    # Parse the data from each row with information we care about, and store it in json format
+    temp_json = '{'
+    for row in history_table_rows:
+        if 'Mean Temperature' in row.get_text():
+            cells = row.findAll('td')
+            temp_json += cell_to_json('Actual Mean Temperature', cells[1])
+            temp_json += cell_to_json('Average Mean Temperature', cells[2])
+        elif 'Max Temperature' in row.get_text():
+            cells = row.findAll('td')
+            temp_json += cell_to_json('Actual Max Temperature', cells[1])
+            temp_json += cell_to_json('Average Max Temperature', cells[2])
+            temp_json += cell_to_json('Record Max Temperature', cells[3])
+        elif 'Min Temperature' in row.get_text():
+            cells = row.findAll('td')
+            temp_json += cell_to_json('Actual Min Temperature', cells[1])
+            temp_json += cell_to_json('Average Min Temperature', cells[2])
+            temp_json += cell_to_json('Record Min Temperature', cells[3], False)
+
+    # Finish building the JSON string and return it
+    return temp_json+'}'
+
+
+def cell_to_json(key, cell, needs_comma=True):
+    """ Docstring """
+    temperature = cell.get_text().strip()
+    temperature = temperature.replace('\xa0', '')
+    temperature = temperature.replace('\n', ' ')
+    return '"%s": "%s"%s' % (key, temperature, ',' if needs_comma else '')
+
+
 if __name__ == '__main__':
-    location = format_location(input('Location: '))
-    print(location)
+    print(scrape_weather_data('https://www.wunderground.com/history/airport/KFTY/2017/10/11/DailyHistory.html?req_city=Atlanta&req_state=GA&req_statename=Georgia&reqdb.zip=30301&reqdb.magic=1&reqdb.wmo=99999'))
