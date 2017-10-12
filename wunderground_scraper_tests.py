4fe93814325eaeea84d8e793e2afb8257c2d6c3d
Cameron Powell <cameronspowell@gmail.com>
2017-10-12 00:06:42 -0400

    Created tests for scrape_weather_data method.
    Created the test_scrape_weather_data test.  This tests to see if when given a valid url which we already know what the response will be, the
    scrape_weather_data function outputs the correct data in json format.
    Created the test_scrape_weather_data_404 test.  This tests the scrape_weather_data method to make sure that if we give it an address which
    leads to a 404 error, it handles this gracefully.
    Created the test_scrape_weather_data_no_table function.  This makes sure that scrape_weather_data doesn't try to get a table from any random
    webpage which we were able to go to.

--- wunderground_scraper_tests.py
+++ wunderground_scraper_tests.py
@@ -28,6 +28,49 @@ class TestWundergroundScraper(unittest.TestCase):
         answer = wunderground_scraper.format_location('   Atlanta, Georgia')
         self.assertEqual(answer, 'Atlanta, Georgia')
 
+    def test_scrape_weather_data(self):
+        """ This tests to see if, when provided with a valid wunderground
+         history url, the scrape weather data function correctly
+         retrieves the page, parses the data, and returns it in a string
+         formatted as a JSON object.
+         """
+        url = 'https://www.wunderground.com/history/airport/KFTY/2017/10/11/' \
+              'DailyHistory.html?req_city=Atlanta&req_state=GA&req_statename=' \
+              'Georgia&reqdb.zip=30301&reqdb.magic=1&reqdb.wmo=99999'''
+        json_answer = wunderground_scraper.scrape_weather_data(url)
+        expected_answer = '{"Actual Mean Temperature": "76°F","Average Mean' \
+                          ' Temperature": "64°F","Actual Max Temperature": ' \
+                          '"86°F","Average Max Temperature": "75°F","Record ' \
+                          'Max Temperature": "104°F (1999)","Actual Min ' \
+                          'Temperature": "66°F","Average Min Temperature": ' \
+                          '"53°F","Record Min Temperature": "32°F (2000)"}'
+        self.assertEqual(json_answer, expected_answer)
+
+    def test_scrape_weather_data_404(self):
+        """ This tests to see if, when provided with an invalid wunderground
+         url which we know will return a 404 error, the scrape weather data
+         function within wunderground_scraper.py will handle the error
+         gracefully and return the error in the json that the user
+         receives.
+         """
+        url = 'https://www.wunderground.com/a404url'
+        answer = wunderground_scraper.scrape_weather_data(url)
+        expected_answer = '{"error": "Received 404 status code for url: ' \
+                          'https://www.wunderground.com/a404url"}'
+        self.assertEqual(answer, expected_answer)
+
+    def test_scrape_weather_data_no_table(self):
+        """ This tests to see if, when provided with a url which does not take
+        us to the wunderground history page, the scrape weather data function
+        realizes that the data table element we're looking for is not on the
+        page and returns the appropriate error string.
+        """
+        url = 'https://www.google.com'
+        answer = wunderground_scraper.scrape_weather_data(url)
+        expected_answer = '{"error": "Received a link which does not contain ' \
+                          'the data we want.  Link received %s"}' % url
+        self.assertEqual(answer, expected_answer)
+
 
 if __name__ == '__main__':
     unittest.main()
