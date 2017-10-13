94a5cb460c69d5a64c31e14aa00730ef06c325be
Cameron Powell <cameronspowell@gmail.com>
2017-10-13 02:12:07 -0400

    Updated module comment to be accurate.

--- wunderground_scraper.py
+++ wunderground_scraper.py
@@ -4,8 +4,7 @@ The purpose of this module is to:
 1) Provide methods to get location and date inputs from the user
 2) Provide methods for scraping requested values off of the wunderground
 results page
-4) Provide methods to convert those results into a JSON object and return them
-to the user
+4) Print the requested data back to the user in JSON format
 5) Provide an entrypoint to the program to execute all this functionality
 
 This module can be run by navigating to the root project directory and running
